4ea478a58f83fdc4c55d670fd11b24fd937b8773
Carlos Aviles <carlosfavilesn@gmail.com>
2023-06-11 22:53:21 -0600

    ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Nav from "./components/Nav/Nav"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 
 
 function App() {
   return (
     <>
-    <div className="bg-zinc-200 w-full h-screen" >
+    <div className="bg-zinc-200 w-full h-screen " >
 
     <Nav />
+    <ItemListContainer mensaje="Hola Mundo"/>
     </div>
     </>
   )
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+
+
+const ItemListContainer = ({mensaje}) => {
+
+return(
+    <div className="">
+        <h2 className="">Item List Container</h2>
+        <hr />
+
+        <p className="">{mensaje}</p>
+    </div>
+)
+
+}
+export default ItemListContainer
