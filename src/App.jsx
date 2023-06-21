392b5007eeb949d378228f9d5313fd0e6494211b
cavilesnunez <carlosfavilesn@gmail.com>
2023-06-21 10:02:23 -0600

    2da entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Header from "./components/Header/Header"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Blog from "./components/Blog/Blog"
 import About from "./components/About/About"
 import Contact from "./components/Contact/Contact"
@@ -13,18 +13,19 @@ function App() {
   return (
 
     <BrowserRouter>
+
         <Header />
+     
         <Routes>
-          <Route path="/" element= { <ItemListContainer />} />
-          <Route path="/products" element= { <ItemListContainer />} />
-          <Route path="/products/:categoryId" element={<ItemListContainer />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/productos/:categoryId" element={<ItemListContainer />} />
           <Route path="/detail/:itemId" element={<ItemDetailContainer />}/>
           <Route path="/blog" element= { <Blog />} />
           <Route path="/about" element= { <About />} />
           <Route path="/contact" element= { <Contact />} /> 
-          <Route path="*" element={ <Error404 /> }  />
+          <Route path="*" element={ <Error404 />}/>
         </Routes>
-    </BrowserRouter>
+        </BrowserRouter>
   )
 }
 
