ab28e6f9ea4f110eb6778ee2f8462f09f0867259
cavilesnunez <carlosfavilesn@gmail.com>
2023-06-21 13:52:44 -0600

    2da entrega

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="flex items-center flex-wrap justify-between w-7xl px-2 sm:px-6 lg:px-8 bg-blue-950">
-      <div className="flex item ">
-        <img src={logo} className="m-1 ml-3 mr-3 p-2"  />
+    <nav className="flex items-center justify-between flex-wrap w-7xl px-2 sm:px-6 lg:px-8 bg-blue-950 md-0">
+      <div className="flex items-center flex-no-shrink text-white mr-6">
+        <img src={logo} className="m-0.5 ml-1.5 mr-1.5 p-2"  />
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
         <Link className="inline-flex items-center justify-center p-2 text-gray-200 hover:text-gray-400" to="/">Inicio</Link>
         <Link className="inline-flex items-center justify-center p-2 text-gray-200 hover:text-gray-400" to="/products">Products</Link>
         <Link className="inline-flex items-center justify-center p-2 text-gray-200 hover:text-gray-400" to="/blog">Blog</Link>
         <Link className="inline-flex items-center justify-center p-2 text-gray-200 hover:text-gray-400" to="/about">About</Link>
         <Link className="inline-flex items-center justify-center p-2 text-gray-200 hover:text-gray-400" to="/contact">Contact</Link>
-
+        </div>
       </div>
         <CarWidget />
 
