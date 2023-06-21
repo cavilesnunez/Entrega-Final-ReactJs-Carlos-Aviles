9bf5cff1aece2dd2a0d0b7d3d65457d6ceaa221e
cavilesnunez <carlosfavilesn@gmail.com>
2023-06-21 13:29:56 -0600

    2da entrega

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import imgabout from "/kunai.jpg"
 
 const About = () => {
   return (
     <div>
             <h2 className="text-center text-2xl font-semibold mt-3">About</h2>
             <hr/>
+
+            <div className="py-16 bg-white">  
+              <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                  <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                    <img src={imgabout} alt="image" className="rounded-lg shadow-2xl"  />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                      <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">KeebMx</h2>
+                      <p className="mt-6 text-gray-600">Comercializamos componentes electrónicos para ensamble de teclados mecánicos. Realizamos envios a todo México. También ofrecemos servicio de ensamble de teclados. Localizamos el teclado de tu preferencia si no lo tenemos en stock. </p>
+                      <p className="mt-4 text-gray-600">Proximamente podrás configurar el teclado de tu preferencia, con los componentes que tu desees. Desde el tipo de case, switch de la especificación que desees y el tipo de keycaps que nos solicites.</p>
+
+                    </div>
+                  </div>
+              </div>
+            </div>
     </div>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,8 +3,36 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-            <h2 className="text-center text-2xl font-semibold mt-3">Contact</h2>
+            <h2 className="text-center text-4xl font-semibold mt-3">Contacto</h2>
             <hr/>
+
+            <div class="flex min-h-screen items-center justify-start bg-white">
+              <div class="mx-auto w-full max-w-lg">
+                
+                <p class="mt-3">Envíenos un correo electrónico a <span className="text-blue-900 hover:text-blue-700 font-bold">contacto@keebmx.com</span> o envíenos un mensaje aquí:</p>
+
+                <form action="" class="mt-10">
+
+                  <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" /> 
+                  <div class="grid gap-6 sm:grid-cols-2">
+                    <div class="relative z-0">
+                      <input type="text" name="name" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                      <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Tu Nombre</label>
+                    </div>
+                    <div class="relative z-0">
+                      <input type="text" name="email" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                      <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Tu correo</label>
+                    </div>
+                    <div class="relative z-0 col-span-2">
+                      <textarea name="message" rows="5" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+                      <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Tu mensaje</label>
+                    </div>
+                  </div>
+                  <button type="submit" class="mt-5 rounded-md bg-blue-900 hover:bg-blue-700 text-white px-10 py-2">Enviar</button>
+                </form>
+              </div>
+            </div>
+
     </div>
   )
 }
