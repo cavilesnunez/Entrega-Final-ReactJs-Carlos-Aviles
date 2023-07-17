aa7ec8f634a1ef6964e3ca4ce9b5c3a1975d0364
cavilesnunez <carlosfavilesn@gmail.com>
2023-07-16 20:27:44 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { CartProvider } from "./context/CartContext"
 import { AuthContextProvider } from "./context/AuthContext"
 import AppRouter from "./router/AppRouter"
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   
